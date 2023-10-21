0e765c8531e03446e5e856ae2a1a8edbc45c9f52
oscar <shuoscaryo@gmail.com>
2023-10-21 18:09:04 +0200

    finished ex04

--- Cpp_01/ex04/main.cpp
+++ Cpp_01/ex04/main.cpp
@@ -6,7 +6,58 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 13:04:59 by orudek            #+#    #+#             */
-/*   Updated: 2023/10/20 13:05:00 by orudek           ###   ########.fr       */
+/*   Updated: 2023/10/21 18:06:07 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
+
+
+void replace_str(std::string &buffer, std::string word, std::string replacement)
+{
+	//try catch puede
+	size_t pos;
+	size_t offset = 0;
+	size_t word_len = word.length();
+	size_t rep_len = replacement.length();
+	do{
+		pos = buffer.find(word, offset);
+		if (pos == std::string::npos)
+			break;
+		buffer.erase(pos, word_len);
+		buffer.insert(pos, replacement);
+		offset = pos + rep_len;
+	}while (1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout<<"Incorrect number of arguments" << std::endl;
+		return (1);
+	}
+	std::ifstream is;
+	std::ofstream os;
+	std::string buffer;
+
+	is.open(argv[1]);
+	if (!is.is_open())
+	{
+		std::cout << "Couldn't open input file \"" << argv[1] << "\"" << std::endl;
+		return (1);
+	}
+	os.open((std::string(argv[1]) + std::string(".replace")).c_str());
+	if (!os.is_open())
+	{
+		std::cout << "Couldn't open output file \"" << argv[1] << ".replace\"" << std::endl;
+		return (1);
+	}
+	while(std::getline(is, buffer))
+	{
+		replace_str(buffer, argv[2], argv[3]);
+		os << buffer << std::endl;
+	}
+	return 0;
+}
