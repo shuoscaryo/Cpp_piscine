65c9cfb4d9f2742876585959a8b413d852135484
oscar <shuoscaryo@gmail.com>
2023-11-28 15:03:43 +0100

    .

--- Cpp_05/ex02/main.cpp
+++ Cpp_05/ex02/main.cpp
@@ -3,22 +3,77 @@
 #include "PresidentialPardonForm.hpp"
 #include "Bureaucrat.hpp"
 
+#define PURPLE "\033[1;35m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+
 int main()
 {
-	Bureaucrat john("John Johnson", 17);
-	Bureaucrat bill("Bill Frontend", 3);
-	Bureaucrat django("Django Fett", 26);
+	{
+		std::cout << PURPLE "TEST: Everything works fine" RESET << std::endl;
+		Bureaucrat bill("Bill Frontend", 3);
 
-	AForm *president = new PresidentialPardonForm("Kali");
-	RobotomyRequestForm robots("Manfred");
-	ShrubberyCreationForm shrub("Tree!");
+		std::cout << YELLOW "Creating the three types of forms" RESET << std::endl;
+		AForm *president = new PresidentialPardonForm("Kali");
+		RobotomyRequestForm robots("Manfred");
+		ShrubberyCreationForm shrub("Tree!");
+		std::cout << "\n";
 
-	bill.signForm(*president);
-	bill.signForm(robots);
-	bill.signForm(shrub);
+		std::cout << YELLOW "signing every form" RESET << std::endl;
+		bill.signForm(*president);
+		bill.signForm(robots);
+		bill.signForm(shrub);
+		std::cout << "\n";
 
-	bill.executeForm(*president);
-	bill.executeForm(robots);
-	bill.executeForm(shrub);
+		std::cout << YELLOW "Executing form PresidentialPardonForm" RESET << std::endl;
+		bill.executeForm(*president);
+		std::cout << "\n";
+		std::cout << YELLOW "Executing form RobotomyRequestForm" RESET << std::endl;
+		bill.executeForm(robots);
+		std::cout << "\n";
+		std::cout << YELLOW "Executing form ShrubberyCreationForm" RESET << std::endl;
+		bill.executeForm(shrub);
+		std::cout << "\n";
+		delete president;
+	}
+	{
+		std::cout << PURPLE "TEST: try to sign and execute with rank too low" RESET << std::endl;
+		Bureaucrat nobody("that guy",150);
+		ShrubberyCreationForm tree("ascii_art");
+		std::cout << YELLOW "rank too low trying to sign form" RESET << std::endl;
+		nobody.signForm(tree);
+		std::cout << "\n";
+
+		std::cout << YELLOW "ok rank trying to sign form" RESET << std::endl;
+		Bureaucrat somebody("some guy", 145);
+		somebody.signForm(tree);
+		std::cout << "\n";
+
+		std::cout << YELLOW "rank too low trying to execute form" RESET << std::endl;
+		nobody.executeForm(tree);
+		std::cout << "\n";
+		std::cout << YELLOW "ok rank trying to execute form" RESET << std::endl;
+		somebody.executeForm(tree);
+		std::cout << "\n";
+	}
+	std::cout << std::endl;
+	{
+		std::cout << PURPLE "TEST: try to execute unsigned form" RESET << std::endl;
+		Bureaucrat Jefrew("Jefrew", 1);
+		PresidentialPardonForm pardon("william");
+
+		std::cout << YELLOW "Try execute form before signing " RESET << std::endl;
+		Jefrew.executeForm(pardon);
+		std::cout << "\n";
+
+		std::cout << YELLOW "Sign form" RESET << std::endl;
+		Jefrew.signForm(pardon);
+		std::cout << "\n";
+		std::cout << YELLOW "Sign form again" RESET << std::endl;
+		Jefrew.signForm(pardon);
+		std::cout << "\n";
+		std::cout << YELLOW "Try execute form again " RESET << std::endl;
+		Jefrew.executeForm(pardon);
+	}
 	return 0;
 }
