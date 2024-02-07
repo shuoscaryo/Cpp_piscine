3a91fde9830c160c3ed4230e0b57defb505ef50d
shuoscaryo <shuoscaryo@gmail.com>
2024-02-07 19:17:37 +0100

    [main]<2024-02-07-19:17> quicksave

--- Cpp_09/ex00/main.cpp
+++ Cpp_09/ex00/main.cpp
@@ -2,6 +2,7 @@
 #include "BitcoinExchange.hpp"
 #include <fstream>
 #include <map>
+#include <exception>
 
 std::string getNextLine(std::string& str)
 {
@@ -14,6 +15,43 @@ std::string getNextLine(std::string& str)
 	return ret;
 }
 
+std::string trim(const std::string& str, const std::string & charset = " \t\n\r\f\v")
+{
+    size_t first = str.find_first_not_of(charset);
+    if (std::string::npos == first)
+        return str;
+    size_t last = str.find_last_not_of(charset);
+    return str.substr(first, (last - first + 1));
+}
+
+std::string readFile(const std::string & file)
+{
+	// Try to open the file
+	std::ifstream fd(file);
+	if (!fd.is_open())
+	{
+		std::cout << "Error: could not open file." << std::endl;
+		throw std::runtime_error("");
+	}
+
+	// save the file into a string
+	std::string ret;
+	fd >> ret;
+	return ret;
+}
+
+std::map<std::string,size_t> loadDatabase(const std::string & file)
+{
+	//main handles the exception
+	std::string fileStr = readFile(file);
+
+	while (!fileStr.empty())
+	{
+		std::string line = getNextLine(fileStr);
+		size_t pos 
+	}
+}
+
 int main(int argc, char **argv)
 {
 	// check if there are 2 arguments
@@ -24,17 +62,14 @@ int main(int argc, char **argv)
 	}
 
 	// try to open the file and save it as a string
-	std::ifstream fd(argv[1]);
-	if (!fd.is_open())
-	{
-		std::cout << "Error: could not open file." << std::endl;
-		return 1;
-	}
 	std::string file;
-	fd >> file;
+	try { file = readFile(argv[1]); }
+	catch (const std::exception & e) { return 1; }
 
 	// Load the database
-	std::map<std::string, int> db = loadDatabase("data.csv");
+	std::map<std::string, size_t> db;
+	try { loadDatabase("data.csv"); }
+	catch (const std::exception & e) { return 1; }
 
 	return 0;
 }
